cf0569f20847d012936d879a82ac518dcb597cb1
Jason Spencer <wolfen231@gmail.com>
2026-05-25 20:43:51 +0800

    feat: wire App.OnStartup to load tags and construct MainViewModel

--- src/SunoMetatagApp/App.xaml.cs
+++ src/SunoMetatagApp/App.xaml.cs
@@ -1,13 +1,31 @@
-﻿using System.Configuration;
-using System.Data;
+using System;
+using System.IO;
 using System.Windows;
+using SunoMetatagApp.Services;
+using SunoMetatagApp.ViewModels;
 
 namespace SunoMetatagApp;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
-}
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
 
+        var tagsPath = Path.Combine(AppContext.BaseDirectory, "tags.json");
+
+        MainViewModel vm;
+        try
+        {
+            var tags = TagService.LoadAll(tagsPath);
+            vm = new MainViewModel(tags);
+        }
+        catch (TagLoadException ex)
+        {
+            vm = new MainViewModel(ex.Message);
+        }
+
+        var window = new MainWindow { DataContext = vm };
+        window.Show();
+    }
+}
